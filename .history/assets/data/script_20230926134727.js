867c55b431a52b826cda704d7c2f4bed85338ce2
casper392945 <casper392945@google.com>
2023-09-26 14:58:39 +0600

    script for search filtering data

---
+++ .history/assets/data/script_20230926134727.js
@@ -0,0 +1,192 @@
+// Получить элемент, куда вы хотите поместить созданный HTML
+// const blog = document.getElementBy("blog");
+// var blogElements = document.getElementsByClassName("blog");
+// var firstBlogElement = blogElements[0];
+var blogElement = document.querySelector(".blog");
+
+let tabs = [];
+let html = "";
+let searchInput = "Studio";
+
+// function fetchData() {
+//   // showPreloader();
+//   fetch("assets/data/tabs2json4blog.json")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // Sort the data
+//       const sortedData = data.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+//       return sortedData;
+//     })
+//     .then((sortedData) => {
+//       // Process the sorted data
+//       // ...
+
+//       // hidePreloader();
+//       return sortedData;
+//     })
+//     .then((sortedData) => {
+//       // Load more items
+//       // ...
+//       loadMoreItems(sortedData);
+//     })
+//     .catch((error) => {
+//       console.error("An error occurred:", error);
+//       // hidePreloader();
+//     });
+// }
+function convertHTMLTags(string) {
+  const regex = /[<>]/g;
+  const htmlEntities = {
+    "<": "&lt;",
+    ">": "&gt;",
+  };
+  return string.replace(regex, (match) => htmlEntities[match]);
+}
+
+function search() {
+  // Get the value from the input field
+  var searchValue = document.getElementById("searchInput").value;
+
+  // Use the search value for further processing, such as filtering or searching data
+  console.log("Search value:", searchValue);
+
+  // Clear the input field
+  document.getElementById("searchInput").value = "";
+  return searchValue;
+}
+
+function fetchData() {
+  fetch("assets/data/tabs2json4blog.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const filteredData = data.filter((item) =>        item["title"].includes(searchInput);        );
+      console.log(filteredData.length);
+      console.log(filteredData);
+      const sortedData = filteredData.sort(
+        (a, b) => new Date(b.date) - new Date(a.date)
+      );
+      // return sortedData;
+    })
+    .then((sortedData) => {
+      tabs = sortedData;
+      loadMoreItems();
+    });
+}
+
+let loadedItems = 0; // Number of items already loaded
+const itemsPerPage = 24; // Number of items to load per page
+
+// Function to load more items from the JSON array
+
+function loadMoreItems() {
+  const remainingItems = tabs.length - loadedItems;
+  const itemsToLoad = Math.min(itemsPerPage, remainingItems);
+
+  // Load the next batch of items from the JSON array
+  const nextItems = tabs.slice(loadedItems, loadedItems + itemsToLoad);
+
+  // Process and display the nextItems as needed
+  nextItems.forEach((item) => {
+    // Process the item and add it to the page
+    // Format date
+    const dateString = item.date;
+    const date = new Date(dateString);
+    const options = {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    };
+
+    let formattedDate = date.toLocaleDateString("en-US", options);
+
+    let title = item["og:title"];
+    if (!title) {
+      title = item.title;
+    }
+    if (!title) {
+      title = "";
+    }
+    title = convertHTMLTags(title);
+
+    let description = item["og:description"];
+    if (!description) {
+      description = item.description;
+    }
+    if (!description) {
+      description = "";
+    }
+    description = convertHTMLTags(description);
+
+    let image = item["og:image"];
+    if (!image) {
+      image = "assets/images/placeholder.png";
+    }
+
+    // Create HTML element
+    html += `
+        <div class="conteudo post-info col-12 col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-2">
+          <div class="post-date">${formattedDate}</div>
+          <img loading="lazy" src=${image} onerror="this.onerror=null; this.src='assets/images/placeholder.png';">
+          <div class="post-hostname">${item.hostname}</div>
+          <h3 class="post-title">
+            <a href="${item.url}" target="_blank">${title}</a>
+          </h3>
+          <div class="post-description">${description}</div>
+      </div>
+    `;
+    // Insert the HTML element into the DOM
+    blogElement.innerHTML = html;
+  });
+  loadedItems += itemsToLoad;
+}
+// // Example: create HTML elements and append them to a container
+// const itemElement = document.createElement("div");
+// itemElement.textContent = item.name;
+// document.getElementById("itemsContainer").appendChild(itemElement);
+
+// Function to check if the user has scrolled to the bottom of the page
+function checkScroll() {
+  // const scrollPosition = window.innerHeight + window.pageYOffset;
+  const scrollPosition = window.innerHeight + window.scrollY;
+  const pageHeight = document.documentElement.scrollHeight;
+
+  if (scrollPosition >= pageHeight) {
+    loadMoreItems();
+  }
+}
+
+// loadMoreItems();
+
+// Attach the checkScroll function to the scroll event
+window.addEventListener("scroll", checkScroll);
+
+fetchData();
+
+// Wrap the initial loading of items in the window load event listener
+// window.addEventListener("load", () => {
+//   fetchData();
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   showPreloader();
+//   // console.log("Page loaded!");
+// });
+
+// Show preloader
+// function showPreloader() {
+//   const preloader = document.getElementById("preloader");
+//   preloader.style.display = "block";
+// }
+
+// Hide preloader
+// function hidePreloader() {
+//   const preloader = document.getElementById("preloader");
+//   preloader.style.display = "none";
+// }
+
+// Function to load data from file
+
+// Call the fetchData function to initiate the data fetching process
+// fetchData();
--- assets/data/script.js
+++ assets/data/script.js
@@ -1,39 +1,43 @@
-// Получить элемент, куда вы хотите поместить созданный HTML
-// const blog = document.getElementBy("blog");
-// var blogElements = document.getElementsByClassName("blog");
-// var firstBlogElement = blogElements[0];
 var blogElement = document.querySelector(".blog");
 
-var tabs = [];
+let tabs = [];
 let html = "";
+let searchInput = "";
+let applyFilter = false; // Flag to determine whether the filter should be applied
 
-// function fetchData() {
-//   // showPreloader();
-//   fetch("assets/data/tabs2json4blog.json")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       // Sort the data
-//       const sortedData = data.sort((a, b) => new Date(b.date) - new Date(a.date));
+function fetchData() {
+  fetch("assets/data/tabs2json4blog.json")
+    .then((response) => response.json())
+    .then((data) => {
+      let filteredData = data; // Initialize filteredData with the original data
 
-//       return sortedData;
-//     })
-//     .then((sortedData) => {
-//       // Process the sorted data
-//       // ...
+      if (applyFilter) {
+        filteredData = data.filter(item =>
+          item["hostname"].includes(searchInput)
+        );
+        console.log(filteredData.length);
+      }
 
-//       // hidePreloader();
-//       return sortedData;
-//     })
-//     .then((sortedData) => {
-//       // Load more items
-//       // ...
-//       loadMoreItems(sortedData);
-//     })
-//     .catch((error) => {
-//       console.error("An error occurred:", error);
-//       // hidePreloader();
-//     });
-// }
+      // Sort the data
+      const sortedData = filteredData.sort(
+        (a, b) => new Date(b.date) - new Date(a.date)
+      );
+
+      return sortedData;
+    })
+    .then((sortedData) => {
+      tabs = sortedData;
+      loadMoreItems();
+    });
+}
+
+// Event listener for search button
+const searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', () => {
+  applyFilter = true; // Enable the filter
+  fetchData();
+});
+
 function convertHTMLTags(string) {
   const regex = /[<>]/g;
   const htmlEntities = {
@@ -55,40 +59,37 @@ function search() {
   return searchValue;
 }
 
+// function fetchData() {
+//   fetch("assets/data/tabs2json4blog.json")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const filteredData = data.filter((item) =>
+//         item["title"].includes(searchInput)
+//       );
+//       // console.log(filteredData.length);
+//       // console.log(filteredData);
+//       const sortedData = filteredData.sort(
+//         (a, b) => new Date(b.date) - new Date(a.date)
+//       );
+//       // console.log(sortedData.length);
+//       // console.log(sortedData);
+//       tabs = sortedData;
+//       // console.log(tabs.length);
+//       // console.log(tabs);
+//       return tabs;
+//     });
+//   loadMoreItems();
+// }
 
-
-function fetchData() {
-  fetch("assets/data/tabs2json4blog.json")
-    .then((response) => response.json())
-    .then((data) => {
-      // Sort the data
-      const sortedData = data.sort(
-        (a, b) => new Date(b.date) - new Date(a.date)
-      );
-      return sortedData;
-    })
-    .then((sortedData) => {
-      tabs = sortedData;
-      loadMoreItems();
-    });
-}
-
-let loadedItems = 0; // Number of items already loaded
-const itemsPerPage = 24; // Number of items to load per page
-
-// Function to load more items from the JSON array
+let loadedItems = 0;
+const itemsPerPage = 24;
 
 function loadMoreItems() {
   const remainingItems = tabs.length - loadedItems;
   const itemsToLoad = Math.min(itemsPerPage, remainingItems);
-
-  // Load the next batch of items from the JSON array
   const nextItems = tabs.slice(loadedItems, loadedItems + itemsToLoad);
 
-  // Process and display the nextItems as needed
   nextItems.forEach((item) => {
-    // Process the item and add it to the page
-    // Format date
     const dateString = item.date;
     const date = new Date(dateString);
     const options = {
@@ -123,7 +124,6 @@ function loadMoreItems() {
       image = "assets/images/placeholder.png";
     }
 
-    // Create HTML element
     html += `
         <div class="conteudo post-info col-12 col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-2">
           <div class="post-date">${formattedDate}</div>
@@ -135,19 +135,13 @@ function loadMoreItems() {
           <div class="post-description">${description}</div>
       </div>
     `;
-    // Insert the HTML element into the DOM
+
     blogElement.innerHTML = html;
   });
   loadedItems += itemsToLoad;
 }
-// // Example: create HTML elements and append them to a container
-// const itemElement = document.createElement("div");
-// itemElement.textContent = item.name;
-// document.getElementById("itemsContainer").appendChild(itemElement);
 
-// Function to check if the user has scrolled to the bottom of the page
 function checkScroll() {
-  // const scrollPosition = window.innerHeight + window.pageYOffset;
   const scrollPosition = window.innerHeight + window.scrollY;
   const pageHeight = document.documentElement.scrollHeight;
 
@@ -156,36 +150,6 @@ function checkScroll() {
   }
 }
 
-// loadMoreItems();
-
-// Attach the checkScroll function to the scroll event
 window.addEventListener("scroll", checkScroll);
 
 fetchData();
-
-// Wrap the initial loading of items in the window load event listener
-// window.addEventListener("load", () => {
-//   fetchData();
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   showPreloader();
-//   // console.log("Page loaded!");
-// });
-
-// Show preloader
-// function showPreloader() {
-//   const preloader = document.getElementById("preloader");
-//   preloader.style.display = "block";
-// }
-
-// Hide preloader
-// function hidePreloader() {
-//   const preloader = document.getElementById("preloader");
-//   preloader.style.display = "none";
-// }
-
-// Function to load data from file
-
-// Call the fetchData function to initiate the data fetching process
-// fetchData();
